da116b2df183b395f7e02bbe845d63dad27c7914
AryanBhardwaj789 <aryanbhardwaj98126@gmail.com>
2024-10-13 02:00:53 -0400

    Integrated OpenAI for further commands

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 import musicLibrary
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# Load environment variables from .env file
+load_dotenv('.env.local')
+# Access the API key from environment variables
+api_key = os.getenv("OPENAI_API_KEY")
+# Initialize the OpenAI client with the API key
+client = OpenAI(api_key=api_key)
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
@@ -9,6 +19,22 @@ engine = pyttsx3.init()
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+
+def aiProcess(command):
+    client = OpenAI(api_key=api_key)
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {   "role": "system", 
+                "content": "You are a virtual assistant named Jarvis skilled in general tasks like Alexa and Google Home. Give short responses please"
+            },
+            {
+                "role": "user",
+                "content": command
+            }
+        ]
+    )
+    return completion.choices[0].message.content
 
 def processCommand(c):
     if "open google" in c.lower():
@@ -23,6 +49,10 @@ def processCommand(c):
         song = c.lower().split(" ")[1]
         link = musicLibrary.music[song]
         webbrowser.open(link)
+    else:
+        # Let OpenAI handle the request
+        output = aiProcess(c)
+        speak(output)
 
 if __name__ == "__main__":
     speak("Initializing Jarvis......")
@@ -35,7 +65,7 @@ if __name__ == "__main__":
         try:
             with sr.Microphone() as source:
                 print("Listening......")
-                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+                audio = r.listen(source, timeout=10, phrase_time_limit=5)
             word = r.recognize_google(audio)
             if(word.lower() == "jarvis"):
                 speak("Ya")
